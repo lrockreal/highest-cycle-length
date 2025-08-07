08f28d18aea4ad04131d9204a9bf38e43fdaac23
Joshua McKinnon <flxwedd@gmail.com>
2025-08-06 18:20:17 -0700

    docs: better variable names and docs

--- src/main.rs
+++ src/main.rs
@@ -6,87 +6,94 @@ const GOAL: usize = 10000;
 const THREADS: usize = 32;
 
 fn main() {
-    let mut results = [0; GOAL - 1];
-    let mut handles = vec![];
+    let mut hcls = [0; GOAL - 1];
+    let mut threads = vec![];
 
-    for t in 0..THREADS {
-        let handle = thread::spawn(move || {
-            let mut results_t = [0; (GOAL - 1).div_ceil(THREADS)];
+    for thread_idx in 0..THREADS {
+        let thread = thread::spawn(move || {
+            let mut thread_hcls = [0; (GOAL - 1).div_ceil(THREADS)];
 
-            let mut i = 0;
-            for x in ((t as u64 + 2)..=(GOAL as u64)).step_by(THREADS) {
-                results_t[i] = highest_cyclength(x);
-                if x % 100 == 0 {
-                    eprintln!("{x}");
+            let mut b_idx = 0;
+            for b in ((thread_idx as u64 + 2)..=(GOAL as u64)).step_by(THREADS) {
+                thread_hcls[b_idx] = highest_cyclength(b);
+                if b % 100 == 0 {
+                    eprintln!("{b}");
                 }
-                i += 1;
+                b_idx += 1;
             }
 
-            eprintln!("Worker {t} finished");
+            eprintln!("Worker {thread_idx} finished");
 
-            results_t
+            thread_hcls
         });
 
-        handles.push(handle);
+        threads.push(thread);
     }
 
-    for (t, h) in handles.into_iter().enumerate() {
-        let results_t = h.join().unwrap();
-        for i in 0..((GOAL - 2 - t) / THREADS + 1) {
-            results[(i * THREADS) + t] = results_t[i];
+    for (thread_idx, thread) in threads.into_iter().enumerate() {
+        let thread_hcls = thread.join().unwrap();
+        for i in 0..((GOAL - 2 - thread_idx) / THREADS + 1) {
+            hcls[(i * THREADS) + thread_idx] = thread_hcls[i];
         }
     }
 
     print!("[");
-    for i in 0..(GOAL - 1) {
-        let m = results[i];
-        if i == GOAL - 2 {
-            println!("{m}]");
+    for b in 0..(GOAL - 1) {
+        let hcl_b = hcls[b];
+        if b == GOAL - 2 {
+            println!("{hcl_b}]");
         } else {
-            print!("{m}, ");
+            print!("{hcl_b}, ");
         }
     }
 }
 
-fn highest_cyclength(x: u64) -> u64 {
-    if is_prime(x) {
-        x - 1
+/// Returns the highest cycle length among all numbers in base `b`.
+fn highest_cyclength(b: u64) -> u64 {
+    if is_prime(b) {
+        b - 1
     } else {
-        let mut m = 1;
-        for i in 2..x {
-            let b = cyclength(i, x);
-            if b > m {
-                m = b;
+        let mut hcl_b = 1;
+        for n in 2..b {
+            let cl_n_base_b = cyclength(n, b);
+            if cl_n_base_b > hcl_b {
+                hcl_b = cl_n_base_b;
             }
         }
-        m
+        hcl_b
     }
 }
 
-fn qmod(n: u64, x: u64, b: u64) -> u64 {
-    let mut r = 1;
+/// Modular exponentiation function.
+/// Calculates `(n^e) mod b`
+fn qmod(n: u64, mut e: u64, b: u64) -> u64 {
+    let mut qmod = 1;
     let mut base = n % b;
-    let mut exp = x;
-    while exp > 0 {
-        if exp % 2 == 1 {
-            r = (r * base) % b;
+    while e > 0 {
+        if e % 2 == 1 {
+            qmod = (qmod * base) % b;
         }
         base = (base * base) % b;
-        exp /= 2;
+        e /= 2;
     }
-    r
+    qmod
 }
 
+/// Returns the cycle length of `n` base `b`
+/// by calculating terms in the sequence:
+/// `a_n = (a_{n-1} * i) mod b`
+/// `a_1 = i`
+/// Starting from `a_1` a repetition is found.
 fn cyclength(n: u64, b: u64) -> u64 {
-    let mut seen = [false; GOAL];
-    let mut x = 1;
+    let mut terms_seen = [false; GOAL];
+    let mut term_idx = 1;
     loop {
-        let qmod = qmod(n, x, b) as usize;
-        if seen[qmod] {
+        let term = qmod(n, term_idx, b) as usize;
+        if terms_seen[term] {
             break;
         }
-        seen[qmod] = true;
-        x += 1;
+        terms_seen[term] = true;
+        term_idx += 1;
     }
-    x - 1
+    term_idx - 1
 }
